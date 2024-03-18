4a7465dc679d7fc9892df92995df032f89392dd7
Pete Bachant <pete@windesco.com>
2024-03-18 14:46:13 -0700

    Get working

--- pyiec61850/setup.py
+++ pyiec61850/setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, Extension
 
 setup(
     name="iec61850",
     version="1.5",
-    py_modules=["iec61850"],
-    package_data={"iec61850": ["_iec61850.so"]}
+    py_modules=["_iec61850"],
+    packages=["iec61850"],
+    include_package_data=True,
+    package_data={"iec61850": ["_iec61850.so"]},
 )
